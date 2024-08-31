ce0645389780b147bc19285f09997561d4233829
Ajay Kumar <ajaysharadkumar@gmail.com>
2024-09-01 00:07:45 +0530

    Release 1 (#1)
    * Here is the very basic calculator which just do simple addition and subtraction

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def addition(x, y):
+    return x + y
+
+
+def subtraction(x, y):
+    return abs(x - y)
+
+
+a = int(input("Enter the value a: "))
+b = int(input("Enter the value b: "))
+
+print(f"Addition of a two number is {addition(a, b)}")
+print(f"Subtraction of a two number is {subtraction(a, b)}")
