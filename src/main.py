18f87204d21f130a70ac431f7b04f6d02d9aec8f
Ajay Kumar <ajaysharadkumar@gmail.com>
2024-09-01 21:46:49 +0530

    Adding Multiplication function

--- src/main.py
+++ src/main.py
@@ -6,8 +6,13 @@ def subtraction(x: int, y: int) -> int:
     return abs(x - y)
 
 
+def multiplication(x: int, y: int) -> int:
+    return x * y
+
+
 a = int(input("Enter the value a: "))
 b = int(input("Enter the value b: "))
 
 print(f"Addition of a two number is {addition(a, b)}")
 print(f"Subtraction of a two number is {subtraction(a, b)}")
+print(f"Multiplication of a two number is {multiplication(a, b)}")
